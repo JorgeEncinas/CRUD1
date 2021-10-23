3da5de1d33e9fa6ac8170d2f2ff94172763139e0
alegre <jorgecarlosencinas@gmail.com>
2021-10-22 20:29:08 -0700

    protected insertar.php

--- insertar.php
+++ insertar.php
@@ -8,18 +8,25 @@ $fecha=$_POST ['fecha'];
 $numero=$_POST ['numero'];
 $numdouble=$_POST ['numdouble'];
 
-$query="INSERT INTO table1 (column1, column2, column3, column4, column5) VALUES ($identificador, '$nombre', '$fecha', $numero, $numdouble);";
+$query="INSERT INTO table1 (column1, column2, column3, column4, column5) VALUES (':id', ':nombre', ':fecha', :numero, :numdouble);";
 
 
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
+$stmt = $conn->prepare($query);
+$stmt->bind_param(":id", $identificador);
+$stmt->bind_param(":nombre", $nombre);
+$stmt->bind_param(":fecha", $fecha);
+$stmt->bind_param(":numero", $numero);
+$stmt->bind_param(":numdouble", $numdouble);
+$stmt->execute();
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-if ( $conn->query($query)){
+if ($stmt->num_rows() > 0){
     header("location:crud.php");
 }
 else{
