cd747b77356f5a9cab418d43d93f161a3b8ee1fd
alegre <jorgecarlosencinas@gmail.com>
2021-10-22 20:25:25 -0700

    protected eliminar.php

--- eliminar.php
+++ eliminar.php
@@ -6,17 +6,20 @@ validar();
 
 $id=$_POST['column1'];
 
-$query="DELETE FROM table1 WHERE column1=$id;";
+$query="DELETE FROM table1 WHERE column1=:id;";
 
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
+$stmt = $conn->prepare($query);
+$stmt->bind_param(":id", $id);
+$stmt->execute();
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-if ( $conn->query($query)){
+if ( $stmt->num_rows() > 0){
     header("location:crud.php");
 }else{
     echo "Algo salio mal <a href='https://localhost/crud.php'> clic aqui para volver al crud</a>" ;
